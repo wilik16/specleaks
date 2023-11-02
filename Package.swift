b66759f8b1cce9eba8cc1b928689a8f61bbe90f3
wilik <wilik@bukalapak.com>
2023-11-02 13:11:47 +0700

    add Nimble. to predicate use quick 5.0.1 use nimble 10.0.0

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
         .library(name: "SpecLeaks", targets: ["SpecLeaks"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Quick/Nimble", from: "9.0.0"),
-        .package(url: "https://github.com/Quick/Quick", from: "3.0.0"),
+        .package(url: "https://github.com/Quick/Nimble", from: "10.0.0"),
+        .package(url: "https://github.com/Quick/Quick", from: "5.0.1"),
     ],
     targets: [     
         .target(name: "SpecLeaks", dependencies: ["Quick", "Nimble"], path: "SpecLeaks/Classes/"),
